f0497edc69a35d83ade473b49aed1b75b8dbcd13
c.r.cano <c.r.cano@outlook.com>
2023-05-17 19:12:37 -0600

    Mejoras al proyecto

--- app/src/main/java/com/example/demo_jp_compose/MainActivity.kt
+++ app/src/main/java/com/example/demo_jp_compose/MainActivity.kt
@@ -28,16 +28,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.demo_jp_compose.models.Message
 import com.example.demo_jp_compose.models.Recipe
 import com.example.demo_jp_compose.models.SampleData
